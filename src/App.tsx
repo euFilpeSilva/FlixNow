186f0292548c6e21e37dbfb1409a597e8174f304
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-04-26 19:24:24 -0300

    corrigindo bug da paginação

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import Navbar from "./components/navbar/Navbar";
-import { Movie } from "./models/Movie";
 import { Home } from "./pages/home/Home";
 import './App.css';
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Carousel from "react-material-ui-carousel";
 import Pagination from "@mui/material/Pagination";
+import { Film } from "./models/Film";
 
-
-
-export type Film = {
-  id: number,
-  title: string,
-  poster_path: string,
-  vote_average: number,
-  genre_ids: number[],
-}
+// export type Film = {
+//   id: number,
+//   title: string,
+//   poster_path: string,
+//   vote_average: number,
+//   genre_ids: number[],
+// }
 
 export type PopularFilms = {
   page: number,
@@ -26,42 +24,42 @@ export type PopularFilms = {
 
 export default function App() {
   const [filmes, setFilmes] = useState<Film[]>([]);
-
   const [page, setPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(6);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
+  const baseURL = `https://api.themoviedb.org/3/movie/popular?api_key=c53174418b2a81eacf8a7966fa850c98&language=pt-BR&page=${page}`;
+  
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const currentPageItems = filmes.slice(start, end);
 
 const handlePageChange = (event: any, newPage: number) => {
   setPage(newPage);
 }
 
-
-    const baseURL = `https://api.themoviedb.org/3/movie/popular?api_key=c53174418b2a81eacf8a7966fa850c98&language=pt-BR&page=${page}`;
-
   useEffect(() => {
     axios.get<PopularFilms>(baseURL).then((response) => {
       setFilmes(response.data.results);
+       console.log(response.data.results);
     });
   }, []);
 
-  const start = (page - 1) * itemsPerPage;
-const end = start + itemsPerPage;
-const currentPageItems = filmes.slice(start, end);
 
 return (
   <div id="principal">
     <Navbar />
     <div className="carousel-container">
-    <Carousel >
+    {/* <Carousel >
       {currentPageItems.map((filme: Film, index: number) => (
         <div key={index} id="imagem-c">
           <img src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`} alt={filme.title} />
         </div>
       ))}
-    </Carousel>
+    </Carousel> */}
     </div>
     <Home filmes={currentPageItems} />
+    
     <Pagination 
-  page={page} 
+  page={page}
   count={Math.ceil(filmes.length / itemsPerPage)} 
   onChange={handlePageChange}
 />

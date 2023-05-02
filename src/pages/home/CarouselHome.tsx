26aa23e195033a0831262f8d5ba3101d76335140
erick-serednicki-pernambuco <epernambuco.erick@gmail.com>
2023-05-01 22:56:14 -0300

    login + recuperar senha

--- src/pages/home/CarouselHome.tsx
+++ src/pages/home/CarouselHome.tsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Film } from '../../models/Film';
-import './carouselHome.css';
+import './CarouselHome.css';
 import { PopularFilms } from '../../App';
 import axios from 'axios';
 import FundoHome from '../../components/fundoHome/FundoHome';
